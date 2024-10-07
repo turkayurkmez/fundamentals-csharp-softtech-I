9fa9c1bd77bae7d35f30506143de983dd54cdcba
türkay ürkmez <turkay.urkmez@outlook.com>
2024-10-07 16:03:58 +0300

    Day 1 completed

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,18 @@
+﻿//Array ve elemanlarını biliyorum
+string[] countries = { "Almanya", "İngiltere", "Türkiye" };
+
+//bellekte 20 elemanlık bir int array'i oluştur:
+//Array'ın 20 elemanlı olduğunu biliyorum. Değerleri bilmiyorum!
+int[] ages = new int[20];
+string[] seasons = new string[] { "Winter", "Spring", "Summer", "Fall" };
+
+Console.WriteLine($"ages dizisinin eleman sayısı: {seasons.Length}");
+Console.WriteLine(seasons[0]);
+Console.WriteLine(seasons[seasons.Length -1]);
+
+
+
+
+
+
+
