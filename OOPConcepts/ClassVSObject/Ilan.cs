21f0ece40bba4b1e17ad36db13b0c7e15db70864
türkay ürkmez <turkay.urkmez@outlook.com>
2024-10-09 15:20:36 +0300

    OOP Started with concept

---
+++ OOPConcepts/ClassVSObject/Ilan.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassVSObject
+{
+    //Bu bir tanım. Sokrates için Idea, Aristotales için Form'dur.
+    public class Ilan
+    {
+
+        //Her ilanın ................ vardır.
+        private string baslik;
+
+        public void SetBaslik(string deger)
+        {
+            if (string.IsNullOrEmpty(deger))
+            {
+                throw new Exception("İlan başlığı boş olamaz");
+            }
+
+            this.baslik = deger;
+        }
+
+        public string GetBaslik()
+        {
+            return this.baslik;
+        }
+
+        //2001
+        private string lokasyon;
+        public string Lokasyon
+        {
+            get { return lokasyon; }
+            set { lokasyon = value; }
+
+        }
+
+        public DateTime IlanTarihi { get; set; }
+        public bool AktifMi { get; set; }
+
+        public List<string> Resimler { get; set; }
+
+
+
+    }
+}
---
+++ OOPConcepts/ClassVSObject/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace ClassVSObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Emlak uygulaması....
+             * 
+             * Bu uygulama ile müşteriler, ilan arayarak diledikleri emlağın ayrıntılarını inceleyecekler.
+             */
+
+            Ilan satilikEv = new Ilan();
+
+            // satilikEv.Baslik = "Sahibinden lüks.....";
+            satilikEv.SetBaslik("Sahibinden lüks.....");
+
+            satilikEv.Lokasyon = "İstanbul";
+
+            Ilan kiralikDukkan = new Ilan();
+            //kiralikDukkan.Baslik = "Zemin kat bakkala uygun";
+            kiralikDukkan.SetBaslik("Zemin kat bakkala uygun");
+            kiralikDukkan.Lokasyon = "Sivas";
+
+            Console.WriteLine($"Dükkan, {kiralikDukkan.Lokasyon} lokasyonunda başlığı ise {kiralikDukkan.GetBaslik()}");
+
+
+            Ilan mustakil = null;
+            //mustakil.Baslik = "üç katlı";
+
+            Ilan yazlik = satilikEv;
+            yazlik.Lokasyon = "Muğla";
+
+            Console.WriteLine($"Satılık evin lokasyonu: {satilikEv.Lokasyon}");
+            Console.WriteLine($"Yazlık evin lokasyonu: {yazlik.Lokasyon}");
+
+            Ilan yeniIlan = new Ilan();
+            Console.WriteLine("İlan başlığını girin");
+            string baslik = Console.ReadLine();
+            //if (string.IsNullOrEmpty(baslik))
+            //{
+            //    throw new Exception("İlan başlığı boş olamaz");
+            //}
+            //yeniIlan.Baslik = baslik;
+            yeniIlan.SetBaslik(baslik);
+            yeniIlan.Lokasyon = "Çanakkale";
+
+            yeniIlan.IlanTarihi = new DateTime(2024, 8, 30);
+            yeniIlan.Resimler = new List<string> { "1.png" };
+          
+
+            Console.WriteLine(yeniIlan.GetBaslik());
+
+
+
+
+        }
+    }
+}
