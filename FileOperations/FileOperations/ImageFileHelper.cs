5fa1394f6227d5843db2da89fe324d0f1dcf97cc
türkay ürkmez <turkay.urkmez@outlook.com>
2024-10-10 16:01:13 +0300

    System.IO operations

---
+++ FileOperations/FileOperations/ImageFileHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOperations
+{
+    public static class ImageFileHelper
+    {
+        public static List<string> FindImages(string path, bool isCreateLogFile = false)
+        {
+            ArgumentNullException.ThrowIfNullOrEmpty("path");
+            string[] files = null;
+            if (!Directory.Exists(path))
+            {
+                throw new DirectoryNotFoundException($"{path} adresinde klasör bulunamadı!");
+            }
+
+            files = Directory.GetFiles(path, "*.png", SearchOption.AllDirectories);
+
+            if (isCreateLogFile)
+            {
+                File.WriteAllLines($"{path}\\log.txt", files);
+            }
+
+            return files.ToList();
+
+
+
+
+        }
+    }
+}
---
+++ FileOperations/FileOperations/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+
+namespace FileOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 1. Bir klasör oluştur
+             * 2. Bu klasörün içine dosya koy.
+             */
+
+            var info = Directory.CreateDirectory("C:\\TestFolder");
+            Console.WriteLine($"{info.CreationTime} anında klasör oluşturuldu");
+            File.WriteAllText($"{info.FullName}\\data.txt", "Bu ifade, uygulama ile oluşturuldu!");
+
+            try
+            {
+                var images = ImageFileHelper.FindImages(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), isCreateLogFile:true);
+                foreach (var image in images)
+                {
+                    Console.WriteLine(image);
+                }
+
+            }
+            catch (DirectoryNotFoundException)
+            {
+
+                Console.WriteLine("Klasör bulunamadı");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Klasör adı belirtilmeli ");
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+
+            }
+
+
+        }
+    }
+}
